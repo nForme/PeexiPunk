7d8226cdc569671030d245aa45eacc29067386e2
nForme <narik229223@mail.ru>
2024-10-10 14:50:22 +0300

    Try

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './styles/Responsive.css'
 import TopNewsBlock from './components/TopNewsBlock/TopNewsBlock'
 import Filter from './components/Filter/Filter'
 import NewsList from './components/NewsList/NewsList'
-import React from 'react'
+// import React from 'react'
 
 function App() {
 
