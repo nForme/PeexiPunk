6a3f6d2670ebab54fc9420f896c0e56cbcd6a079
nForme <narik229223@mail.ru>
2024-10-21 14:13:17 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./PeexiPunk",
+  base: "/PeexiPunk",
   plugins: [react()],
 
   css: {
